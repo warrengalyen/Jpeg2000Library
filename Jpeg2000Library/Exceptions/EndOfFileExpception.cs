9b30b265dc7a2afb782b69b1b20072176b99c179
Warren Galyen <wgalyen@gmail.com>
2020-07-03 11:02:52 -0700

    Implemented wrapper for stream access

---
+++ Jpeg2000Library/Exceptions/EndOfFileExpception.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jpeg2000Library.Exceptions
+{
+    public class EndOfFileExpception : Exception
+    {
+        public EndOfFileExpception() : base("End of file reached!")
+        {
+
+        }
+    }
+}
---
+++ Jpeg2000Library/IO/IRandomAccessIO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jpeg2000Library.IO
+{
+    public interface IRandomAccessIO
+    {
+        long Position { get; }
+        long Length { get; }
+        void Seek(long offset);
+        uint ReadInt();
+        ushort ReadShort();
+        ulong ReadLong();
+    }
+}
