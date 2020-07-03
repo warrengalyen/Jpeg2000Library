13fb718c8a457e97043daaf588dad3285e2a8e62
Warren Galyen <wgalyen@gmail.com>
2020-07-03 11:52:38 -0700

    Added parameterlist

--- Jpeg2000Library/Decoder.cs
+++ Jpeg2000Library/Decoder.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.IO;
+using Jpeg2000Library.CodeStream;
+using Jpeg2000Library.CodeStream.Reader;
 using Jpeg2000Library.Exceptions;
 using Jpeg2000Library.FileFormat.Reader;
 using Jpeg2000Library.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.Metadata;
 
 namespace Jpeg2000Library
 {
-    public class Decoder
+    public class ParameterList : IReadOnlyDictionary<string, object>
     {
+        public ParameterList(IDictionary<string, object> parameters)
+        {
+            _backingDict = parameters != null ? new Dictionary<string, object>(parameters) : new Dictionary<string, object>();
+        }
+        private Dictionary<string, object> _backingDict;
+        public object this[string key] => _backingDict.ContainsKey(key) ? _backingDict[key] : null;
 
-        public Decoder() { }
+        public IEnumerable<string> Keys => _backingDict.Keys;
 
-        public Image Decode(IRandomAccessIO input)
+        public IEnumerable<object> Values => _backingDict.Values;
+
+        public int Count => _backingDict.Count;
+
+        public bool ContainsKey(string key) => _backingDict.ContainsKey(key);
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => _backingDict.GetEnumerator();
+
+        public bool TryGetValue(string key, out object value) => _backingDict.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() => _backingDict.GetEnumerator();
+
+        public bool Debug => ((string)this["debug"] ?? "on") == "on";
+
+    }
+
+    public class Decoder 
+    { 
+        public ParameterList ParameterList { get; private set; }
+        public Decoder(IDictionary<string, object> parameterList = null)
+        {
+            ParameterList = new ParameterList(parameterList);
+        }
+
+        public void Decode(IRandomAccessIO input)
         {
             var fileFormatReader = new FileFormatReader(input);
             if (fileFormatReader.JP2FFUsed)
@@ -19,7 +55,29 @@ namespace Jpeg2000Library
                 input.Seek(fileFormatReader.FirstCodeStreamPosition);
             }
 
-            return null;
+            // **** Header decoder ****
+            // Instantiate header decoder and read main header
+            var headerInfo = new HeaderInfo();
+            HeaderDecoder headerDecoder = null;
+            try
+            {
+                headerDecoder = new HeaderDecoder(input, ParameterList, headerInfo);
+            }
+            catch (EndOfFileException e)
+            {
+                Logger.Error("Codestream too short or bad header, unable to decode.");
+                if (ParameterList.Debug)
+                {
+                    Logger.Warning(e.StackTrace);
+                }
+                else
+                {
+                    Logger.Error("Use '-debug' option for more details");
+                }
+                return;
+            }
+
+            // return null
         }
     }
 }
--- Jpeg2000Library/Program.cs
+++ Jpeg2000Library/Program.cs
@@ -1,55 +1,55 @@
 ﻿using System;
 using System.IO;
-using Jpeg2000Library.Codecs;
 using Jpeg2000Library.IO;
 
 namespace Jpeg2000Library
 {
-    class Program
+   
+    public class Logger
     {
-        public class Logger
+        public enum Severity
         {
-            public enum Severity
-            {
-                Error, 
-                Warning,
-                Debug
-            }
-            public static Severity MaxSeverity = Severity.Debug;
-            public static void Log(Severity severity, string message)
-            {
-                if (severity > MaxSeverity)
-                    return;
+            Error, 
+            Warning,
+            Debug
+        }
+        public static Severity MaxSeverity = Severity.Debug;
+        public static void Log(Severity severity, string message)
+        {
+            if (severity > MaxSeverity)
+                return;
 
-                switch (severity)
-                {
-                    case Severity.Error:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        break;
-                    case Severity.Warning:
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        break;
-                    default:
-                        break;
-                }
-                System.Console.WriteLine(message);
-                Console.ResetColor();
-            }
-
-            public static void Error(string message)
+            switch (severity)
             {
-                Log(Severity.Error, message);
+                case Severity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Severity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                default:
+                    break;
             }
-            public static void Warning(string message)
-            {
-                Log(Severity.Warning, message);
-            }
-            public static void Debug(string message)
-            {
-                Log(Severity.Debug, message);
-            }
+            System.Console.WriteLine(message);
+            Console.ResetColor();
         }
 
+        public static void Error(string message)
+        {
+            Log(Severity.Error, message);
+        }
+        public static void Warning(string message)
+        {
+            Log(Severity.Warning, message);
+        }
+        public static void Debug(string message)
+        {
+            Log(Severity.Debug, message);
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
             Console.Clear();
@@ -71,7 +71,8 @@ namespace Jpeg2000Library
 
                 var decoder = new Decoder();
                 var randomAccessFile = new RandomAccessFile(inputFile);
-                var decoded = decoder.Decode(randomAccessFile);
+                //var decoded = decoder.Decode(randomAccessFile);
+                decoder.Decode(randomAccessFile);
                 //// todo: different codecs
                 //codec codec = null;
                 //switch (false)
