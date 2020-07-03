6436598815237cb9cee9dd0a7684b5e174040638
Warren Galyen <wgalyen@gmail.com>
2020-07-03 10:48:39 -0700

    Created initial dotnet core project

---
+++ Jpeg2000Library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jpeg2000Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
